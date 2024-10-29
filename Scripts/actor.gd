9330f88b9690685f82645275b8f37a91c80c9b18
Madi Simpson <madibyee@gmail.com>
2024-10-29 14:21:06 -0700

    shaders: better transparency shader after some research

--- Scripts/actor.gd
+++ Scripts/actor.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 @onready var overworld_level: OverworldLevel
 var controls_locked: bool = false
-var move_time: float = 0.25
+var move_time: float = (4.0/15.0)
 @export var direction: Vector2 = Vector2.DOWN
 
 func interact(_player: OverworldPlayer):
